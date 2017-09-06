bde08d78f3c810aa26ac371153da07275ed9f154
Renan Muniz <renan@renanmuniz.com>
2017-09-06 15:06:20 -0300

    Fix PDF margin size (fix #4)
    Update PDF margin to: (top: 0.39", right: 0.38", bottom: 0.38", left: 0.39")

--- pdf.js
+++ pdf.js
@@ -1,6 +1,13 @@
 const path = require('path')
 const puppeteer = require('puppeteer')
 
+const CHROME_DEFAULT_PDF_A4_MARGIN = {
+		top: '0.39in',
+		right: '0.38in',
+		bottom: '0.38in',
+		left: '0.39in'
+}
+
 const init = async () => {
 		const buildPath = path.join(__dirname, '/build')
 		const pageURL = `file:/\/\/${buildPath}/index.html`
@@ -9,7 +16,11 @@ const init = async () => {
 	  const page = await browser.newPage()
 	  await page.goto(pageURL, {waitUntil: 'networkidle'})
 		await page.waitFor(2000)
-	  await page.pdf({path: 'resumé.pdf', format: 'A4'})
+	  await page.pdf({
+			path: 'resumé.pdf',
+			format: 'A4',
+			margin: CHROME_DEFAULT_PDF_A4_MARGIN
+		})
 
 	  browser.close()
 }
